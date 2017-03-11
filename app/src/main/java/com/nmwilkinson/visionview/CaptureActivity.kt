c6515f1db901ccbceca9499a20e530fabafea5ae
neil <nmwilkinson@gmail.com>
2017-03-11 15:51:38 +0000

    Started sample app - need to handle detections in the UI.

---
+++ app/src/main/java/com/nmwilkinson/visionview/CaptureActivity.kt
@@ -0,0 +1,186 @@
+package com.nmwilkinson.visionview
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.text.TextBlock
+import com.nmwilkinson.vision_detector_view.VisionCameraConfig
+import com.nmwilkinson.vision_detector_view.VisionDetectorConfig
+import kotlinx.android.synthetic.main.activity_capture.*
+
+class CaptureActivity : AppCompatActivity(), VisionDetectorConfig.ErrorCallback {
+
+    private lateinit var captureViewConfig: VisionCameraConfig
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture)
+
+        setTitle(if (getDetectorType() == VisionDetectorConfig.DetectorType.OCR) R.string.ocr else R.string.barcode)
+
+        takePicture.setOnClickListener {
+            capture.takePicture({
+            }, {
+                val message = "${resources.getString(R.string.picture_taken)} of size ${it.size / 1024}KB"
+                Snackbar.make(rootLayout, message, Snackbar.LENGTH_SHORT).show()
+            })
+        }
+
+        captureViewConfig = VisionCameraConfig.Builder()
+                .setAutoFocus(intent.getBooleanExtra(OPTION_AUTOFOCUS, true))
+                .setUseFlash(intent.getBooleanExtra(OPTION_FLASH, true))
+                .setFps(2.0f)
+                .build()
+
+        // Check for the camera permission before accessing the camera.  If the
+        // permission is not granted yet, request permission.
+        val rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            capture.createCameraSource(applicationContext, captureViewConfig, createDetectorConfig())
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private val ocrProcessor = object : Detector.Processor<TextBlock> {
+        override fun receiveDetections(detections: Detector.Detections<TextBlock>?) {
+        }
+
+        override fun release() {
+        }
+    }
+    private val barcodeProcessor = object : Detector.Processor<Barcode> {
+        override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
+        }
+
+        override fun release() {
+        }
+    }
+
+    private fun createDetectorConfig(): VisionDetectorConfig {
+        return when (getDetectorType()) {
+            VisionDetectorConfig.DetectorType.OCR -> VisionDetectorConfig.createOcr(ocrProcessor, this)
+            VisionDetectorConfig.DetectorType.BARCODE -> VisionDetectorConfig.createBarcode(barcodeProcessor, this)
+            else -> throw IllegalStateException("Unhandled detector type ${getDetectorType()}")
+        }
+    }
+
+    private fun getDetectorType() = VisionDetectorConfig.DetectorType.values()[intent.getIntExtra(OPTION_MODE, 0)]
+
+    override fun onResume() {
+        super.onResume()
+        startCameraSource()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        capture.stopCapturing()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        capture.shutdown()
+    }
+
+    override fun onError(errorCode: Int) {
+        val message = when (errorCode) {
+            VisionDetectorConfig.ErrorCallback.ERROR_DEPENDENCIES_NOT_DOWNLOADED -> R.string.low_storage_error
+            else -> R.string.unknown_error
+        }
+        Snackbar.make(capture, message, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, {})
+                .show()
+    }
+
+    private fun requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission")
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM)
+            return
+        }
+
+        Snackbar.make(capture, R.string.permission_camera_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, {
+                    ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM)
+                })
+                .show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode)
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source")
+            // We have permission, so create the camerasource
+            capture.createCameraSource(applicationContext, captureViewConfig, createDetectorConfig())
+            return
+        }
+
+        Log.e(TAG, "Permission not granted: results len = ${grantResults.size} Result code = " + if (grantResults.isNotEmpty()) grantResults[0] else "(empty)")
+
+        val listener = DialogInterface.OnClickListener { dialog, int -> finish() }
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.app_name))
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show()
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists.  If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    @Throws(SecurityException::class)
+    private fun startCameraSource() {
+        // Check that the device has play services available.
+        val code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(applicationContext)
+        if (code != ConnectionResult.SUCCESS) {
+            val dlg = GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS)
+            dlg.show()
+        }
+
+        capture.startCapturing()
+    }
+
+    companion object {
+        private const val OPTION_MODE = "4t5yflr"
+        private const val OPTION_AUTOFOCUS = "gb90voi3kl"
+        private const val OPTION_FLASH = "n9iok4d"
+
+        private val TAG = CaptureActivity::class.java.simpleName
+
+        private val permissions = arrayOf(Manifest.permission.CAMERA)
+        private val RC_HANDLE_CAMERA_PERM = 2
+        private val RC_HANDLE_GMS = 9001
+
+        fun launchIntent(context: Context, mode: VisionDetectorConfig.DetectorType, autofocus: Boolean, flash: Boolean): Intent {
+            val intent = Intent(context, CaptureActivity::class.java)
+            intent.putExtra(OPTION_MODE, mode.ordinal)
+            intent.putExtra(OPTION_AUTOFOCUS, autofocus)
+            intent.putExtra(OPTION_FLASH, flash)
+            return intent
+        }
+    }
+}
---
+++ app/src/main/java/com/nmwilkinson/visionview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nmwilkinson.visionview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nmwilkinson.vision_detector_view.VisionDetectorConfig.DetectorType.BARCODE
+import com.nmwilkinson.vision_detector_view.VisionDetectorConfig.DetectorType.OCR
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        ocr.isChecked = true
+
+        autofocus.isChecked = true
+
+        ocr.setOnClickListener { if (ocr.isChecked) barcode.isChecked = false }
+        barcode.setOnClickListener { if (barcode.isChecked) ocr.isChecked = false }
+
+        start.setOnClickListener {
+            val mode = if (ocr.isChecked) OCR else BARCODE
+            startActivity(CaptureActivity.launchIntent(this, mode, autofocus.isChecked, flash.isChecked))
+        }
+    }
+}
